37db4a210de9b740a4348dee7375743f316c20b0
Rod <oscar.aguilar1@catolica.edu.sv>
2025-02-18 17:57:29 -0600

    otro commit

--- L01_2022AA650_2022AJ650/Controllers/publicacionesController.cs
+++ L01_2022AA650_2022AJ650/Controllers/publicacionesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using L01_2022AA650_2022AJ650.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace L01_2022AA650_2022AJ650.Controllers
@@ -7,5 +8,30 @@ namespace L01_2022AA650_2022AJ650.Controllers
     [ApiController]
     public class publicacionesController : ControllerBase
     {
+        private readonly ClaseContext _publicacionesContexto;
+
+        public publicacionesController(ClaseContext publicacionesContexto)
+        {
+            _publicacionesContexto = publicacionesContexto;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<publicaciones> publicaciones = (from l in _publicacionesContexto.publicaciones
+                                  select l).ToList();
+            if (publicaciones.Count() == 0)
+            {
+                return NotFound();
+
+            }
+
+            return Ok(publicaciones);
+        }
+
+
+
+
     }
 }
