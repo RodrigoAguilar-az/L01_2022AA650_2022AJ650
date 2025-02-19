013ab6cc807ba9096e556fa94a10dc86d8d05e66
asensio01 <guillermo.asensio@catolica.edu.sv>
2025-02-18 18:02:07 -0600

    crud usuarios

--- L01_2022AA650_2022AJ650/Controllers/usuariosController.cs
+++ L01_2022AA650_2022AJ650/Controllers/usuariosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using L01_2022AA650_2022AJ650.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace L01_2022AA650_2022AJ650.Controllers
@@ -7,5 +8,44 @@ namespace L01_2022AA650_2022AJ650.Controllers
     [ApiController]
     public class usuariosController : ControllerBase
     {
+        private readonly  ClaseContext _claseContext;
+
+        public usuariosController(ClaseContext claseContext) { 
+        
+         _claseContext = claseContext;
+        }
+
+        //Metodo para obtener todos los usarios
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<usuarios> listadoUsuarios = (from e in _claseContext.Usuarios
+                                              select e).ToList();
+            if (listadoUsuarios.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoUsuarios);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult GuardarUsario([FromBody] usuarios usuarios)
+        {
+            try
+            {
+                _claseContext.Usuarios.Add(usuarios);
+                _claseContext.SaveChanges();
+                return Ok(usuarios);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
     }
 }
