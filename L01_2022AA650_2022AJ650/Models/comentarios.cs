691695b1070ccdde92a0b26be9a68a50c83b2dc7
Rod <oscar.aguilar1@catolica.edu.sv>
2025-02-18 19:10:47 -0600

    commit casi final

--- L01_2022AA650_2022AJ650/Models/comentarios.cs
+++ L01_2022AA650_2022AJ650/Models/comentarios.cs
@@ -5,6 +5,9 @@ namespace L01_2022AA650_2022AJ650.Models
     public class comentarios
     {
         [Key]
-        public int pubcometarioIdlicacionId { get; set; }
+        public int cometarioId { get; set; }
+        public int publicacionId { get; set; }
+        public string comentario { get; set; }
+        public int usuarioId { get; set; }
     }
 }
